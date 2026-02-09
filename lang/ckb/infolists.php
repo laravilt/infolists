baf8ebd574ae3a2b4f2ff265dbab38d4724b3050
Swara <swara09@gmail.com>
2026-02-09 11:06:49 +0100

    Add Kurdish (ckb) language support for infolists

---
+++ lang/ckb/infolists.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    // Entries
+    'entries' => [
+        'empty' => 'هیچ تایەک نیە',
+        'loading' => 'بارکردن...',
+        'boolean_true' => 'بەڵێ',
+        'boolean_false' => 'نەخێر',
+    ],
+
+    // Sections
+    'sections' => [
+        'collapse' => 'نووشتانه‌وه‌',
+        'expand' => 'فراوانکردن',
+    ],
+
+    // Actions
+    'actions' => [
+        'copy' => 'لەبەرگرتنەوە',
+        'copied' => 'لەبەرگیرایەوە!',
+        'open_link' => 'کردنەوەی بەستەر',
+        'download' => 'داگرتن',
+        'view' => 'بینین',
+    ],
+
+    // Labels
+    'labels' => [
+        'created_at' => 'زیادکرا',
+        'updated_at' => 'نوێکرایەوە',
+        'deleted_at' => 'سڕایەوە',
+    ],
+];
